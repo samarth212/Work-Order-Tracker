a61f853ac52bdc74f682ed14ac44ff585c032fb7
Samarth Kolanupaka <samarth212@gmail.com>
2026-05-27 20:57:30 -0400

    routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { WorkOrderList } from './pages/work-order-list/work-order-list';
+import { WorkOrderDetail } from './pages/work-order-detail/work-order-detail';
+import { WorkOrderForm } from './pages/work-order-form/work-order-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'workorders',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: 'workorders',
+    component: WorkOrderList,
+  },
+  {
+    path: 'workorders/new',
+    component: WorkOrderForm,
+  },
+  {
+    path: 'workorders/:id',
+    component: WorkOrderDetail,
+  },
+];
