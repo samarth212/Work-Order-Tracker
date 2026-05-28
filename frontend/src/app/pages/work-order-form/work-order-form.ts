e608cfa67f176bbd755d133aa076d455c81cf7a8
Samarth Kolanupaka <samarth212@gmail.com>
2026-05-28 12:20:31 -0400

    fix form submit

--- frontend/src/app/pages/work-order-form/work-order-form.ts
+++ frontend/src/app/pages/work-order-form/work-order-form.ts
@@ -1,32 +1,46 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { WorkOrder } from '../../models/work-order';
-import { FormsModule } from '@angular/forms';
-import { JsonPipe } from '@angular/common';
+import { WorkOrderService } from '../../services/work-order';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-work-order-form',
-  imports: [RouterLink, FormsModule, JsonPipe],
+  imports: [RouterLink, FormsModule],
   templateUrl: './work-order-form.html',
   styleUrl: './work-order-form.css',
 })
 export class WorkOrderForm {
+  constructor(private workOrderService: WorkOrderService) {}
+
   priorities = ['Low', 'Medium', 'High', 'Urgent'] as const;
 
-  newWorkOrder: Partial<WorkOrder> = {
-    title: '',
-    description: '',
-    status: 'Open',
-    priority: this.priorities[0],
-    assignedTo: '',
-    createdBy: '',
-    createdAt: '',
-    dueDate: '',
-  };
+  newWorkOrder: Omit<WorkOrder, 'id'> = this.createEmptyWorkOrder();
+  submittedWorkOrder?: WorkOrder;
 
   submitted = false;
 
-  onSubmit() {
+  onSubmit(form: NgForm) {
+    this.submittedWorkOrder = this.workOrderService.addWorkOrder(this.newWorkOrder);
     this.submitted = true;
+    this.newWorkOrder = this.createEmptyWorkOrder();
+    form.resetForm(this.newWorkOrder);
+  }
+
+  addAnother() {
+    this.submitted = false;
+  }
+
+  private createEmptyWorkOrder(): Omit<WorkOrder, 'id'> {
+    return {
+      title: '',
+      description: '',
+      status: 'Open',
+      priority: this.priorities[0],
+      assignedTo: '',
+      createdBy: '',
+      createdAt: '',
+      dueDate: '',
+    };
   }
 }
