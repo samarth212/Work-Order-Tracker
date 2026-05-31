1cf4bfc669cbac004c01c6168bcfb2a722f998e7
Samarth Kolanupaka <samarth212@gmail.com>
2026-05-30 21:44:34 -0400

    update status option in detail

--- frontend/src/app/pages/work-order-detail/work-order-detail.ts
+++ frontend/src/app/pages/work-order-detail/work-order-detail.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { WorkOrder } from '../../models/work-order';
 import { WorkOrderService } from '../../services/work-order';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-work-order-detail',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './work-order-detail.html',
   styleUrl: './work-order-detail.css',
 })
 export class WorkOrderDetail {
   workOrder: WorkOrder | undefined;
+  statuses = ['Open', 'In Progress', 'Completed'] as const;
 
   constructor(
     private route: ActivatedRoute,
